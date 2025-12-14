7bd207c39bbcbd16b95e7fe372294e62c4047895
FPato <patrik.filipiak1@gmail.com>
2025-12-14 19:39:58 +0100

    first version with classes and relations

--- plugins/drawScriptBuilder.js
+++ plugins/drawScriptBuilder.js
@@ -13,11 +13,11 @@ function jsonToDrawioXml(diagram) {
   const cells = [];
 
   const classesAndInterfaces = [...diagram.classes, ...diagram.interfaces];
-
+  //const classesAndInterfaces = [];
   // Classes
   for (const cls of classesAndInterfaces) {
     const name = Object.keys(cls)[0];
-    //console.log(cls);
+    //console.log(name);
     const xValue = cls[name].layout.x;
     const yValue = cls[name].layout.y;
     const widthValue = cls[name].layout.width;
@@ -26,28 +26,35 @@ function jsonToDrawioXml(diagram) {
     // Class cell
     cells.push(`
       <mxCell id="${esc(name)}" value="${esc(name)}"
-              style="${classStyle}"
-              vertex="1" parent="1">
+              style="${classStyle}fontSize=${diagram.layoutConfig.charWidth};"
+              vertex="1" parent="class_diagram">
         <mxGeometry x="${xValue}" y="${yValue}"
                     width="${widthValue}" height="${heightValue}"
                     as="geometry"/>
       </mxCell>`.trim());
 
+    var y = 30;
+    const increment = diagram.layoutConfig.lineHeight;
 
     // Attributes
     for (const attribute of cls[name].attributes) {
       const attributeName = Object.keys(attribute)[0];
+      //console.log(esc(name + "_" + attributeName));
         cells.push(`
         <mxCell id="${esc(name + "_" + attributeName)}" value="${esc(attributeName + ':' + attribute[attributeName].type)}"
-                style="${attributeStyle}"
+                style="${attributeStyle}fontSize=${diagram.layoutConfig.charWidth};"
                 vertex="1" parent="${esc(name)}">
-            <mxGeometry y="26" width="160" height="26" as="geometry"/>
+            <mxGeometry y="${y}" width="${widthValue}" height="${increment}" as="geometry"/>
         </mxCell>`.trim());
+        y += increment;
     }
+
+    y += 5; // Small gap between attributes and methods
 
     // Methods
     for (const method of cls[name].methods) {
       const methodName = Object.keys(method)[0];
+      //console.log(esc(name + "_" + methodName + "()"));
       var parameters = "";
       for (const param of method[methodName].parameters) {
         const paramName = Object.keys(param)[0];
@@ -58,25 +65,60 @@ function jsonToDrawioXml(diagram) {
       }
 
       cells.push(`
-        <mxCell id="${esc(name + "_" + methodName)}" value="${esc(methodName + '(' + parameters + '):' + method[methodName].returnType)}"
-                style="${methodStyle}"
+        <mxCell id="${esc(name + "_" + methodName + "()")}" value="${esc(methodName + '(' + parameters + '):' + method[methodName].returnType)}"
+                style="${methodStyle}fontSize=${diagram.layoutConfig.charWidth};"
                 vertex="1" parent="${esc(name)}">
-          <mxGeometry y="52" width="160" height="26" as="geometry"/>
+          <mxGeometry y="${y}" width="${widthValue}" height="${increment}" as="geometry"/>
         </mxCell>`.trim());
+        y += increment;
     }
-
   }
+
+    // Associations
+  for (const relations of diagram.relationEndpoints.associations) {
+    //const from = Object.keys(relations)[0];
+    //console.log(relations.pFrom.x);
+    const associationName = "association" + relations.from + "_" + relations.to;
+    cells.push(`
+        <mxCell id="${esc(associationName)}" value="" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;endArrow=open;endFill=0;strokeColor=#666666;" parent="class_diagram" source="${esc(relations.from)}" target="${esc(relations.to)}" edge="1">
+          <mxGeometry x="-0.0909" y="9" relative="1" as="geometry">
+            <Array as="points">
+              <mxPoint x="${esc(relations.pFrom.x)}" y="${esc(relations.pFrom.y)}" />
+              <mxPoint x="${esc(relations.pTo.x)}" y="${esc(relations.pTo.y)}" />
+            </Array>
+            <mxPoint as="offset" />
+          </mxGeometry>
+        </mxCell>`.trim());
+  }
+
+    // Inheritances
+  for (const relations of diagram.relationEndpoints.inheritances) {
+    const inheritanceName = "inheritance" + relations.from + "_" + relations.to;
+    cells.push(`
+        <mxCell id="${esc(inheritanceName)}" value="" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;endArrow=blockThin;endFill=0;strokeColor=#666666;" parent="class_diagram" source="${esc(relations.from)}" target="${esc(relations.to)}" edge="1">
+          <mxGeometry x="-0.0909" y="9" relative="1" as="geometry">
+            <Array as="points">
+              <mxPoint x="${esc(relations.pFrom.x)}" y="${esc(relations.pFrom.y)}" />
+              <mxPoint x="${esc(relations.pTo.x)}" y="${esc(relations.pTo.y)}" />
+            </Array>
+            <mxPoint as="offset" />
+          </mxGeometry>
+        </mxCell>`.trim());
+  }
+
   return `
 <mxGraphModel>
   <root>
     <mxCell id="0"/>
     <mxCell id="1" parent="0"/>
+    <mxCell id="class_diagram" value="classDiagram" parent="0"/>
     ${cells.join("\n    ")}
   </root>
 </mxGraphModel>`.trim();
 }
 
-const raw = readFileSync("layout-test.json", "utf8");
+
+const raw = readFileSync("adit-java-karin-out.json", "utf8");
 const diagram = JSON.parse(raw);
 const res = jsonToDrawioXml(diagram);
 //console.log(res);
