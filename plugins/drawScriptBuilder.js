69a05fe2497d4297e67d8d81359bdd1e234661f3
FPato <patrik.filipiak1@gmail.com>
2025-12-14 23:33:02 +0100

    class diagram done

--- plugins/drawScriptBuilder.js
+++ plugins/drawScriptBuilder.js
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import { readFileSync } from "node:fs";
 
-const classStyle = "swimlane;fontStyle=2;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;rounded=0;shadow=0;strokeWidth=1;editable=1;movable=1;resizable=1;rotatable=1;deletable=1;locked=0;connectable=1;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;"
+const classStyle = "swimlane;fontStyle=2;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;rounded=0;shadow=0;strokeWidth=1;editable=1;movable=1;resizable=1;rotatable=1;deletable=1;locked=0;connectable=1;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;paddingTop=10;"
 const attributeStyle = "text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=1;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;editable=1;movable=1;resizable=1;deletable=1;locked=0;connectable=1;fontStyle=0;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;";
 const methodStyle ="text;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=1;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;editable=1;movable=1;resizable=1;deletable=1;locked=0;connectable=1;fontStyle=0;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;"; 
 
@@ -13,11 +13,9 @@ function jsonToDrawioXml(diagram) {
   const cells = [];
 
   const classesAndInterfaces = [...diagram.classes, ...diagram.interfaces];
-  //const classesAndInterfaces = [];
   // Classes
   for (const cls of classesAndInterfaces) {
     const name = Object.keys(cls)[0];
-    //console.log(name);
     const xValue = cls[name].layout.x;
     const yValue = cls[name].layout.y;
     const widthValue = cls[name].layout.width;
@@ -39,7 +37,6 @@ function jsonToDrawioXml(diagram) {
     // Attributes
     for (const attribute of cls[name].attributes) {
       const attributeName = Object.keys(attribute)[0];
-      //console.log(esc(name + "_" + attributeName));
         cells.push(`
         <mxCell id="${esc(name + "_" + attributeName)}" value="${esc(attributeName + ':' + attribute[attributeName].type)}"
                 style="${attributeStyle}fontSize=${diagram.layoutConfig.charWidth};"
@@ -49,12 +46,12 @@ function jsonToDrawioXml(diagram) {
         y += increment;
     }
 
-    y += 5; // Small gap between attributes and methods
+    if (cls[name].attributes.length > 0)
+        y += 5; // Small gap between attributes and methods
 
     // Methods
     for (const method of cls[name].methods) {
       const methodName = Object.keys(method)[0];
-      //console.log(esc(name + "_" + methodName + "()"));
       var parameters = "";
       for (const param of method[methodName].parameters) {
         const paramName = Object.keys(param)[0];
@@ -76,8 +73,6 @@ function jsonToDrawioXml(diagram) {
 
     // Associations
   for (const relations of diagram.relationEndpoints.associations) {
-    //const from = Object.keys(relations)[0];
-    //console.log(relations.pFrom.x);
     const associationName = "association" + relations.from + "_" + relations.to;
     cells.push(`
         <mxCell id="${esc(associationName)}" value="" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;endArrow=open;endFill=0;strokeColor=#666666;" parent="class_diagram" source="${esc(relations.from)}" target="${esc(relations.to)}" edge="1">
@@ -118,8 +113,7 @@ function jsonToDrawioXml(diagram) {
 }
 
 
-const raw = readFileSync("adit-java-karin-out.json", "utf8");
-const diagram = JSON.parse(raw);
-const res = jsonToDrawioXml(diagram);
-//console.log(res);
-fs.writeFileSync("dummy.drawio", res, "utf8");
+//const raw = readFileSync("adit-java-karin-out.json", "utf8");
+//const diagram = JSON.parse(raw);
+//const res = jsonToDrawioXml(diagram);
+//fs.writeFileSync("dummy.drawio", res, "utf8");
